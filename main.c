03b8ea0c6a9e4f3daaa055bfa672cbeb22b1cbdf
Afnan <Afnanmo95@gmail.com>
2025-12-07 11:35:13 +0300

    show short message when no numbers provided

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (argc < 2) {
-        printf("Usage: %s num1 num2 ...\n", argv[0]);
+        printf("No numbers provided. Please pass integers to calculate the sum.\n");
         return 1;
     }
 
