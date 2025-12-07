1b4f81ebdedd44d772f383c8b9fb152291ede668
Afnan <Afnanmo95@gmail.com>
2025-12-07 11:31:49 +0300

    add sum calculation to main.c

--- main.c
+++ main.c
@@ -10,5 +10,18 @@ int main(int argc, char *argv[]) {
         printf("argv[%d] = %s\n", i, argv[i]);
     }
 
+    if (argc < 2) {
+        printf("Usage: %s num1 num2 ...\n", argv[0]);
+        return 1;
+    }
+
+    int sum = 0;
+
+    for (int i = 1; i < argc; i++) {
+        sum += atoi(argv[i]);
+    }
+
+    printf("Sum = %d\n", sum);
+
     return 0;
 }
